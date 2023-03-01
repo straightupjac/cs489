ea9ae7ba63ccf7958d90dbd11235b55cd43e1631
straightupjac <jdc.jaclyn@gmail.com>
2023-03-01 18:18:46 -0500

    cs489 course work

---
+++ week3/output/CMakeFiles/CappedRandom.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CappedRandom.dir/input.c.o"
+  "CMakeFiles/CappedRandom.dir/input.c.o.d"
+  "CMakeFiles/CappedRandom.dir/main.c.o"
+  "CMakeFiles/CappedRandom.dir/main.c.o.d"
+  "CMakeFiles/CappedRandom.dir/random.c.o"
+  "CMakeFiles/CappedRandom.dir/random.c.o.d"
+  "CappedRandom"
+  "CappedRandom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CappedRandom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week3/output/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/input.c.o"
+  "CMakeFiles/program.dir/input.c.o.d"
+  "CMakeFiles/program.dir/main.c.o"
+  "CMakeFiles/program.dir/main.c.o.d"
+  "CMakeFiles/program.dir/random.c.o"
+  "CMakeFiles/program.dir/random.c.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
